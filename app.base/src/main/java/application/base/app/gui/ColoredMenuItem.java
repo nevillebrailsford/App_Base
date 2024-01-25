e83b054dfa2bc2154baaadf60fb9d5db53a5102b
Neville Brailsford <50483768+nevillebrailsford@users.noreply.github.com>
2024-01-25 10:42:11 +0000

    Removed code from ColoredMenu and ColoredMenuItem

--- app.base/src/main/java/application/base/app/gui/ColoredMenuItem.java
+++ app.base/src/main/java/application/base/app/gui/ColoredMenuItem.java
@@ -3,14 +3,10 @@ package application.base.app.gui;
 import javax.swing.Action;
 import javax.swing.JMenuItem;
 
-import application.definition.ApplicationConfiguration;
-
 public class ColoredMenuItem extends JMenuItem {
 	private static final long serialVersionUID = 1L;
 
 	public ColoredMenuItem(Action action) {
 		super(action);
-		setOpaque(true);
-		setBackground(ApplicationConfiguration.applicationDefinition().bottomColor().get());
 	}
 }
