3f2236e964d16ec34501015df98d6984cf0fc202
Neville Brailsford <neville_brailsford@hotmail.com>
2025-11-22 11:03:51 +0000

    Set default button for password entry

--- app.base/src/main/java/application/base/app/gui/PasswordEntry.java
+++ app.base/src/main/java/application/base/app/gui/PasswordEntry.java
@@ -2,36 +2,33 @@ package application.base.app.gui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
-import application.security.DBAccess;
-import application.security.DBUser;
 import application.security.PasswordEncryptor;
+import application.security.PasswordHandler;
 
-public class LoginFormGUI extends JDialog {
+public class PasswordEntry extends JDialog {
 	private static final long serialVersionUID = 1L;
 
 	private final JPanel contentPanel;
-	private JTextField usernameField;
 	private JPasswordField passwordField;
+	private JButton authorizeButton;
+	private JButton cancelButton;
 
-	public LoginFormGUI(String title) {
+	public PasswordEntry(String title) {
+
 		super();
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setTitle(title);
-		contentPanel = new JPanel();
+		contentPanel = new ColoredPanel();
 		setLayout(new BorderLayout(10, 10));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		add(contentPanel, BorderLayout.CENTER);
@@ -43,71 +40,70 @@ public class LoginFormGUI extends JDialog {
 	}
 
 	private void addGUIComponents() {
-		JLabel usernameLabel = new JLabel("Username:");
-		usernameField = new JTextField();
-		contentPanel.add(usernameLabel);
-		contentPanel.add(usernameField);
-
 		JLabel passwordLabel = new JLabel("Password:");
 		passwordField = new JPasswordField();
+		passwordField.getDocument().addDocumentListener(new DocumentListener() {
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				validatePassword();
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				validatePassword();
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				validatePassword();
+			}
+		});
 
 		contentPanel.add(passwordLabel);
 		contentPanel.add(passwordField);
 
-		JButton loginButton = new JButton("Login");
-		loginButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String username = usernameField.getText();
-				String password = new String(passwordField.getPassword());
-				try {
-					String encryptedPassword = PasswordEncryptor.instance().encrypt(password);
-					if (DBAccess.validateLogin(username, encryptedPassword)) {
-						DBUser.instance().setUsername(usernameField.getText());
-						DBUser.instance().setPassword(encryptedPassword);
-						setVisible(false);
-						LoginFormGUI.this.dispose();
-					} else {
-						DBUser.instance().setUsername(null);
-						DBUser.instance().setPassword(null);
-						JOptionPane.showMessageDialog(LoginFormGUI.this, "Login Failed...please register");
-					}
-				} catch (Exception e1) {
-					JOptionPane.showMessageDialog(LoginFormGUI.this, "Exception caught " + e1.getMessage());
-				}
+		authorizeButton = new JButton("Authorize");
+		authorizeButton.setEnabled(false);
+		authorizeButton.addActionListener(e -> {
+			if (validPassword()) {
+				closeWindow();
 			}
 		});
-		contentPanel.add(loginButton);
+		contentPanel.add(authorizeButton);
+		getRootPane().setDefaultButton(authorizeButton);
 
-		JButton cancelButton = new JButton("Cancel");
-		cancelButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				DBUser.instance().setUsername(null);
-				DBUser.instance().setPassword(null);
-				setVisible(false);
-				LoginFormGUI.this.dispose();
-			}
+		cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(e -> {
+			closeWindow();
 		});
 		contentPanel.add(cancelButton);
-		JLabel registerLabel = new JLabel("Not a user? Register Here.");
-		registerLabel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				LoginFormGUI.this.dispose();
-
-				new RegisterFormGUI("Register").setVisible(true);
-			}
-		});
-		contentPanel.add(registerLabel);
 	}
 
-	public void setUsername(String username) {
-		usernameField.setText(username);
+	private void closeWindow() {
+		setVisible(false);
+		PasswordEntry.this.dispose();
 	}
 
-	public void setPassword(String password) {
-		passwordField.setText(password);
+	private void validatePassword() {
+		if (passwordField.getPassword().length >= 8) {
+			authorizeButton.setEnabled(true);
+		} else {
+			authorizeButton.setEnabled(false);
+		}
 	}
 
+	private boolean validPassword() {
+		try {
+			String checkPassword = PasswordEncryptor.instance().encrypt(String.valueOf(passwordField.getPassword()));
+			return PasswordHandler.instance().validatePassword(checkPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public String getPassword() throws Exception {
+		return PasswordEncryptor.instance().encrypt(String.valueOf(passwordField.getPassword()));
+	}
 }
